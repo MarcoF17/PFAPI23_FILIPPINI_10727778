ac5fef25656230aea62f9565593fd627254c6107
marcolino <marco1.filippini@mail.polimi.it>
2023-06-26 17:27:20 +0200

    "aggiungi-stazione", "aggiungi-auto", "demolisci-stazione", "rottama-auto"

--- main.c
+++ main.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <values.h>
 
 #define POINTER_VECTOR_DIM 100
 
@@ -18,10 +17,7 @@ typedef struct ServiceStation{
 
 typedef struct ServiceStation* SSPointer;
 
-int scanfTmp = 0;
-
-//typedef char String[];
-
+int scanfTmp = 0, stationNumber = 0;
 SSPointer pointerVector[POINTER_VECTOR_DIM];   //vector to optimize access to stations
 
 ///initialize pointer-vector to NULL
@@ -41,51 +37,168 @@ void station_inorder_insert(SSPointer newStation){
 
     SSPointer actualStation = pointerVector[0];
     while(newStation->distance > actualStation->distance){
+        if(newStation->distance == actualStation->distance){
+            printf("non aggiunta\n");
+            free(newStation);
+            return;
+        }
+        actualStation = actualStation->nextStation;
+        if(actualStation == NULL)
+            break;
+    }
+
+    if(actualStation != NULL){
+        newStation->nextStation = actualStation->nextStation;
+        if(newStation->nextStation == NULL)
+            pointerVector[POINTER_VECTOR_DIM-1] = newStation;
+        else
+            newStation->nextStation->prevStation = newStation;
+
+        actualStation->nextStation = newStation;
+        newStation->prevStation = actualStation;
+        if(newStation->prevStation == NULL)
+            pointerVector[0] = newStation;
+    }
+    else{
+        newStation->prevStation = pointerVector[POINTER_VECTOR_DIM-1];
+        newStation->nextStation = NULL;
+        newStation->prevStation->nextStation = newStation;
+        pointerVector[POINTER_VECTOR_DIM-1] = newStation;
+    }
+
+    //printf("aggiunta\n");
+}
+
+///returns the reference to the station with
+SSPointer search_for_station(int distance){
+    SSPointer actualStation = pointerVector[0];
+
+    while(actualStation != NULL){
+        if(actualStation->distance == distance)
+            return actualStation;
         actualStation = actualStation->nextStation;
     }
 
-    newStation->nextStation = actualStation->nextStation;
-    if(newStation->nextStation == NULL)
-        pointerVector[POINTER_VECTOR_DIM-1] = newStation;
-    actualStation->nextStation = newStation;
-    newStation->prevStation = actualStation;
-    if(newStation->prevStation == NULL)
-        pointerVector[0] = newStation;
-    newStation->nextStation->prevStation = newStation;
+    return NULL;
 }
 
 ///in-order insertion of a new car in a station
-void insert_inorder_car(SSPointer station, int battery) {
+int create_insert_inorder_car(SSPointer station, int battery) {
+    if(station == NULL)
+        return 0;       //0 stands for "non aggiunta"
+
     Car *newCar = malloc(sizeof(Car));
     newCar->battery = battery;
+    newCar->nextCar = NULL;
     Car *actualCar = station->carList;
     Car *actualCarPrev = NULL;
-    while (newCar->battery < actualCar->battery) {
-        actualCarPrev = actualCar;
-        actualCar = actualCar->nextCar;
+
+    if(actualCar == NULL)
+        station->carList = newCar;
+
+    else {
+        while (newCar->battery < actualCar->battery) {
+            actualCarPrev = actualCar;
+            actualCar = actualCar->nextCar;
+            if(actualCar == NULL)
+                break;
+        }
+
+        newCar->nextCar = actualCar;
+        if(actualCarPrev != NULL)
+            actualCarPrev->nextCar = newCar;
+        else
+            station->carList = newCar;
     }
 
-    if(actualCarPrev == NULL)
-        station->carList = newCar;
-    else
-        actualCarPrev->nextCar = newCar;
-
-    newCar->nextCar = actualCar;
+    return 1;       //1 stands for "aggiunta"
 }
 
 ///memory allocation for a new station and possibly new cars
 void create_station(int distance, int carsNumber){
+    if(search_for_station(distance) != NULL){
+        printf("non aggiunta\n");
+        return;
+    }
+
     SSPointer tmp;
     tmp = malloc(sizeof(ServiceStation));
     tmp->distance = distance;
+    tmp->carList = NULL;
     station_inorder_insert(tmp);
 
-    if(carsNumber == 0)
+    if(carsNumber > 0){
+        //adding cars list to newStation
+        int i, battery;
+        for(i = 0; i < carsNumber; i++){
+            scanfTmp = scanf("%d", &battery);
+            create_insert_inorder_car(tmp, battery);
+        }
+    }
+
+    printf("aggiunta\n");
+    stationNumber++;
+}
+
+///deletes a station which is at the given distance (if it exists)
+void demolish_station(int distance){
+    SSPointer pointer = search_for_station(distance);
+    if(pointer == NULL){
+        printf("non demolita\n");
         return;
+    }
 
-    int i;
-    for(i = 0; i < carsNumber; i++){
+    if(pointer->prevStation != NULL)
+        pointer->prevStation->nextStation = pointer->nextStation;
+    if(pointer->nextStation != NULL)
+        pointer->nextStation->prevStation = pointer->prevStation;
 
+    Car *actualCar = pointer->carList, *nextCar;
+    while(actualCar != NULL){
+        nextCar = actualCar->nextCar;
+        free(actualCar);
+        actualCar = nextCar;
+    }
+
+    printf("demolita\n");
+    stationNumber--;
+    if(stationNumber == 0){
+        pointerVector[0] = pointerVector[POINTER_VECTOR_DIM-1] = NULL;
+    }
+}
+
+///scraps the car with the given battery capacity (if it exists)
+void scrap_car(int distance, int battery){
+    SSPointer pointer = search_for_station(distance);
+    if(pointer == NULL){
+        printf("non rottamata\n");
+        return;
+    }
+
+    Car *actualCar = pointer->carList, *prevCar = NULL;
+    while(actualCar != NULL){
+        if(actualCar->battery == battery){
+            if(prevCar != NULL)
+                prevCar->nextCar = actualCar->nextCar;
+            else
+                pointer->carList = actualCar->nextCar;
+
+            free(actualCar);
+            printf("rottamata\n");
+            return;
+        }
+        actualCar = actualCar->nextCar;
+    }
+
+    printf("non rottamata\n");
+}
+
+///just for debug
+void print_cars_batteries(SSPointer station){
+    Car *actualCar = station->carList;
+    while(actualCar != NULL){
+        printf("%d\n", actualCar->battery);
+        actualCar = actualCar->nextCar;
     }
 }
 
@@ -99,23 +212,47 @@ int main() {
         if(commandType == 'a'){ //aggiungi-auto || aggiungi-stazione
             while(commandType != '-')
                 scanfTmp = scanf("%c", &commandType);
+
             scanfTmp = scanf("%c", &commandType);
 
             if(commandType == 'a'){     //aggiungi-auto
-
+                while(commandType != ' ')
+                    scanfTmp = scanf("%c", &commandType);
+                int distance, battery;
+                scanfTmp = scanf("%d %d", &distance, &battery);
+                int res = create_insert_inorder_car(search_for_station(distance), battery);
+                if(res == 0)
+                    printf("non aggiunta\n");
+                else
+                    printf("aggiunta\n");
             }
 
             else if(commandType == 's'){    //aggiungi-stazione
+                while(commandType != ' ')
+                    scanfTmp = scanf("%c", &commandType);
 
+                int distance, carsNumber;
+                scanfTmp = scanf("%d %d", &distance, &carsNumber);
+                create_station(distance, carsNumber);
+                //print_cars_batteries(search_for_station(distance));
             }
         }
 
         else if(commandType == 'd'){    //demolisci-stazione
-
+            while(commandType != ' ')
+                scanfTmp = scanf("%c", &commandType);
+            int distance;
+            scanfTmp = scanf("%d", &distance);
+            demolish_station(distance);
         }
 
         else if(commandType == 'r'){    //rottama-auto
+            while(commandType != ' ')
+                scanfTmp = scanf("%c", &commandType);
 
+            int distance, battery;
+            scanfTmp = scanf("%d %d", &distance, &battery);
+            scrap_car(distance, battery);
         }
 
         else if(commandType == 'p'){    //pianifica-percorso
